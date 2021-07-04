53a5e0b05284a1a80bc7116d8d8c0f9f745541c3
Lloyd Brookes <lloyd.brookes@gmail.com>
2021-07-04 20:56:14 +0100

    add dist and index file

---
+++ bin/npms.mjs
@@ -0,0 +1,8 @@
+import NpmApiClient from '@75lb/npm-api/npms-api'
+import fetch from 'node-fetch'
+
+const api = new NpmApiClient({ fetch })
+
+const [method, ...args] = process.argv.slice(2)
+const result = await api[method](...args)
+console.log(JSON.stringify(result, null, '  '))
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import NpmDownloads from './lib/downloads.mjs'
+import NpmRegistry from './lib/registry.mjs'
+import NpmsApi from './lib/registry.mjs'
+
+export { NpmDownloads, NpmRegistry, NpmsApi }
